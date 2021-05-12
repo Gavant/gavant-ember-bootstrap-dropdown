11d811c6d5ed16295c650525540f03bf2a95a574
Adam Baker <bakerac4@gmail.com>
2021-05-12 08:55:03 -0400

    Upgrade via ember-cli-upgrade

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,15 +1,15 @@
 import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import config from 'dummy/config/environment';
 
 export default class Router extends EmberRouter {
-  location = config.locationType;
-  rootURL = config.rootURL;
+    location = config.locationType;
+    rootURL = config.rootURL;
 }
 
-Router.map(function() {
-    this.route('test', function() {
-        this.route('two', {path: "/:two"}, function() {
-          this.route('three', { path: "/:three"});
+Router.map(function () {
+    this.route('test', function () {
+        this.route('two', { path: '/:two' }, function () {
+            this.route('three', { path: '/:three' });
         });
     });
 });
