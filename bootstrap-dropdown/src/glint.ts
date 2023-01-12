06433d40aef2c7cfa3eaa2fdbe2bb286d5fdc608
Adam Baker <bakerac4@gmail.com>
2023-01-12 11:02:11 -0500

    Fixing dropdown menu glint component

--- bootstrap-dropdown/src/glint.ts
+++ bootstrap-dropdown/src/glint.ts
@@ -1,6 +1,6 @@
 // Easily allow apps, which are not yet using strict mode templates, to consume your Glint types, by importing this file.
 // Add all your components, helpers and modifiers to the template registry here, so apps don't have to do this.
-import { BasicDropdownSignature } from 'ember-basic-dropdown/components/basic-dropdown';
+import BasicDropdown from 'ember-basic-dropdown/components/basic-dropdown';
 
 // See https://typed-ember.gitbook.io/glint/using-glint/ember/authoring-addons
 import DropdownAction from 'components/dropdown-action';
@@ -10,6 +10,6 @@ declare module '@glint/environment-ember-loose/registry' {
     export default interface Registry {
         DropdownAction: typeof DropdownAction;
         DropdownLink: typeof DropdownLink;
-        DropdownMenu: BasicDropdownSignature;
+        DropdownMenu: typeof BasicDropdown;
     }
 }
